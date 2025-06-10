854f22f5d4b1a8153c6e4d70d92b1f016f410121
vanaf25 <vanayfefilov777@gmail.com>
2025-06-10 16:31:00 +0200

    jobManagement page

--- src/app/theme/theme.ts
+++ src/app/theme/theme.ts
@@ -4,24 +4,28 @@ import { Inter, Open_Sans } from 'next/font/google';
 
 const openSans = Open_Sans({ subsets: ['latin'] });
 const inter = Inter({ subsets: ['latin'] });
+
 declare module '@mui/material/styles' {
     interface Palette {
         gray: {
-            200:string
+            200: string;
             300: string;
             500: string;
+            600:string;
             700: string;
-            800:string;
+            800: string;
+            900:string;
         };
     }
-
     interface PaletteOptions {
         gray?: {
-            200:string,
+            200: string;
             300: string;
             500: string;
+            600:string;
             700: string;
-            800:string;
+            800: string;
+            900:string
         };
     }
 }
@@ -29,7 +33,7 @@ declare module '@mui/material/styles' {
 const theme = extendTheme({
     typography: {
         fontFamily: inter.style.fontFamily,
-        fontSize:14,
+        fontSize: 14,
         h1: {
             fontWeight: 700,
         },
@@ -38,10 +42,84 @@ const theme = extendTheme({
         MuiButton: {
             styleOverrides: {
                 root: {
-                    color:"white",
                     fontFamily: inter.style.fontFamily,
                     fontWeight: 600,
-                    textTransform: 'none',
+                    fontSize:14,
+                    textTransform: "none",
+                },
+                containedPrimary: {
+                    color: "#fff", // белый цвет текста для кнопок с color="primary" и variant="contained"
+                },
+                textPrimary: {
+                    color: "#fff", // белый текст для text кнопок с color="primary" (если нужно)
+                },
+            },
+        },
+
+        MuiOutlinedInput: {
+            styleOverrides: {
+                root: {
+                    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "secondary.main",
+                    },
+                },
+
+            },
+        },
+
+        MuiSelect: {
+            styleOverrides: {
+                outlined: {
+                    "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                        borderColor: "secondary.main",
+                    },
+                },
+            },
+        },
+
+        MuiCheckbox: {
+            defaultProps: {
+                color: "secondary",
+            },
+            styleOverrides: {
+                root: {
+                    color: "#D4D7DD", // gray.300 default color
+                    "&.Mui-checked": {
+                        color: "secondary.main",
+                    },
+                },
+            },
+        },
+
+        MuiRadio: {
+            defaultProps: {
+                color: "secondary",
+            },
+            styleOverrides: {
+                root: {
+                    color: "#D4D7DD",
+                    "&.Mui-checked": {
+                        color: "secondary.main",
+                    },
+                },
+            },
+        },
+        MuiSwitch: {
+            defaultProps: {
+                color: "secondary",
+            },
+            styleOverrides: {
+                switchBase: {
+                    color: "#D4D7DD",
+                    "&.Mui-checked": {
+                        color: "secondary.main",
+                    },
+                },
+                track: {
+                    backgroundColor: "#D4D7DD",
+                    ".Mui-checked &": {
+                        backgroundColor: "secondary.main",
+                    },
                 },
             },
         },
@@ -50,20 +128,22 @@ const theme = extendTheme({
         light: {
             palette: {
                 primary: {
-                    main: '#F97316',
+                    main: "#F97316",
+                    light: "#FFEDD5",
                 },
                 secondary: {
-                    main: '#FFEDD5',
+                    main: "#7D5AE2",
                 },
                 gray: {
-                    200:"#E4E4E5",
-                    300: '#D4D7DD',
-                    500: '#727885',
-                    700: '#3E4654',
-                    800:"#272E39",
+                    200: "#E4E4E5",
+                    300: "#D4D7DD",
+                    500: "#727885",
+                    600:"#525B67",
+                    700: "#3E4654",
+                    800: "#272E39",
+                    900:"#151B28",
                 },
-              text:{
-              },
+                text: {},
             } as any,
         },
     },
